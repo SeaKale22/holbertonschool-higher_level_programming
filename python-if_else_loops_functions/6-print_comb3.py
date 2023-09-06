6a54052fa85a8f4c539e7c3f8772b8d12332eef8
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-06 09:20:13 -0700

    6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for first in range(10):
     for second in range(first + 1, 10):
-        print("{:d}{:d}".format(first, second),\
-                end=', ' if first < 8 or second < 9 else '\n')
+        print("{:d}{:d}".format(first, second),
+        end=', ' if first < 8 or second < 9 else '\n')
