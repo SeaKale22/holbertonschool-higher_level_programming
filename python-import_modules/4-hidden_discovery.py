443e7a9406336abc6ad32ce47c8babfd58fc4a79
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-07 17:48:16 -0700

    4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,6 @@ if __name__ == "__main__":
     import hidden_4
     module_names = dir(hidden_4)
     filtered_names = [name for name in module_names if not name.startswith("__")]
+    filtered_names.sort()
     for name in filtered_names:
         print(name)
