c166043e343e8614e01d6658defc7cc6604e4f6b
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-11 11:53:59 -0700

    created uniq_add to add all unique elements of a list on ints

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    seen = set()
+    for num in my_list:
+        if num not in seen:
+            seen.add(num)
+            result += num
+    return result
