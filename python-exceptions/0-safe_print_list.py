b25f09cd69a2876e1282d04cec6e1dfa5f6745db
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-12 10:51:35 -0700

    fixed for loop outside of try

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num_printed = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end='')
             num_printed += 1
-    except:
-        pass
-    finally:
-        print()
+        except IndexError:
+            pass
+    print()
     return num_printed
