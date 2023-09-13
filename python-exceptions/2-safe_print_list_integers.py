20f86a3d18cbec1ca899a6f9e0169f4f79bd5fb2
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-13 12:52:27 -0700

    created 2-safe_print_list_integers to print a list of integers up to x, IndexError can occur

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    ints_printed = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end = '')
+            ints_printed += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return ints_printed
