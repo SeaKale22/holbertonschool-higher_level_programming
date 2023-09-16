ec16f201ac5a6d12d27559836dd93502e233b5e1
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-16 12:25:46 -0700

    added padded quotes with space

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This module contains a Square"""
+""" This module contains a Square """
 
 
 class Square:
-    """represents a Square"""
+    """ represents a Square """
     pass
