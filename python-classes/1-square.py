4c66baed1553d0f8bdc6208e2b313aecb998217b
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-17 08:06:59 -0700

    created 1-square that is a class with a private size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This modual contains a square"""
+
+
+class Square:
+    """Represets a square"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
+        """size of the square"""
