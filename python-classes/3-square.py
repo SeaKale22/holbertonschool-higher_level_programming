bde3124c002127ad2b0670f26ebd9d15fcc04bb5
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-17 08:36:16 -0700

    3-square has method for area

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This modual contains a square"""
+
+
+class Square:
+    """Represets a square"""
+    def __init__(self, size=0):
+        """Instantiation with size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Finds the area of the square.
+        Returns: The area of the square.
+        """
+        return self.__size ** 2
