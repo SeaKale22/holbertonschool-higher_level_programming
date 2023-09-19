8ea541bcee833240ae5366ced2aefdeea0f52e8a
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-19 12:45:58 -0700

    fized for pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,12 +8,15 @@ def matrix_divided(matrix, div):
     param div: must be a number (integer or float)
     """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)\
+                of integers/floats")
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)\
+                of integers/floats")
     for row in matrix:
         if not all(isinstance(element, (int, float)) for element in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists)\
+                    of integers/floats")
     first_row_size = len(matrix[0])
     for row in matrix:
         if len(row) != first_row_size:
@@ -22,5 +25,6 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    new_matrix = [[round(element / div, 2) for element in row]
+                  for row in matrix]
     return new_matrix
