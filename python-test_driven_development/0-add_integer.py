1e31816ce8baa531a4989487ef9f55492e751a5d
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-19 11:27:18 -0700

    created add_integer.py and add_integer.txt to test

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module for add function"""
+def add_integer(a, b=98):
+    """
+    Add two numbers
+    param a: first int
+    param b: second int, defult 98
+    raises: TypeError if a or b are not an int or float
+    return: the sum of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
