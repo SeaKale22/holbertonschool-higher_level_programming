142a6e6fcb3ce61ccce32bc08a209f614d0662b0
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-20 08:45:33 -0700

    changed print behavior when position[1] > 0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -55,7 +55,7 @@ class Square:
             for y in range(self.__position[1]):
                 print()
             for x in range(self.__size):
-                if self.__position[1] < 0:
+                if self.__position[1] > 0:
                     print("" * self.__position[0] + "#" * self.__size)
                 else:
                     print(" " * self.__position[0] + "#" * self.__size)
