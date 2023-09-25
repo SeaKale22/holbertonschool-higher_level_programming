3307690c02b477f464654f361f143eccfd462324
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-25 10:54:46 -0700

    put BaseGeometry class in the same module instead of importing it

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,7 +2,19 @@
 """Module for rectangle"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry():
+    """An incomplete class"""
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates an integer"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
 class Rectangle(BaseGeometry):
     """Represents a rectangle and inherits from BaseGeometry"""
     def __init__(self, width, height):
