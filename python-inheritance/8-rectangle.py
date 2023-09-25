ff7360bd2246a1d5c77e648128c5629dcd6f555e
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-25 10:59:06 -0700

    switched back to importing BaseGeometry class and fixed for pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,21 +2,11 @@
 """Module for rectangle"""
 
 
-class BaseGeometry():
-    """An incomplete class"""
-    def area(self):
-        """raises exception"""
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def integer_validator(self, name, value):
-        """Validates an integer"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle and inherits from BaseGeometry"""
+    """Represents a rectangle and inherits BaseGeometry"""
     def __init__(self, width, height):
         """initilization with width and height"""
         self.integer_validator("width", width)
