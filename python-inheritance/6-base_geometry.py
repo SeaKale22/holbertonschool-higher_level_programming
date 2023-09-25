379b84860d42332d6052f7803f79906c7c74817f
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-25 09:51:54 -0700

    added def for area() but did not implement it

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry"""
+
+
+class BaseGeometry():
+    """An incomplete class"""
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
