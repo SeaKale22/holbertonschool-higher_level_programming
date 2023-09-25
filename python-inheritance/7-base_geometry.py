81b8a8cf0a4b34a4ca0aa160e0fda03997736291
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-25 10:17:16 -0700

    created BaseGeometry class with integer_validator and test cases

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry"""
+
+
+class BaseGeometry():
+    """An incomplete class"""
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates an integer"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
