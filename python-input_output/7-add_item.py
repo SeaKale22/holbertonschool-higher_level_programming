eb3d81c1308b5d106085fd4ca2efe8d9f3535f8e
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-26 15:21:01 -0700

    changed to no longer import os

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,17 +3,16 @@
 
 
 import sys
-import os
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arguments = sys.argv[1:]
 
-if not os.path.exists("add_item.json"):
-    add_list = arguments
-    save_to_json_file(add_list, "add_item.json")
-else:
+try:
     add_list = load_from_json_file("add_item.json")
-    add_list.extend(arguments)
-    save_to_json_file(add_list, "add_item.json")
+except FileNotFoundError:
+    add_list = []
+
+add_list.extend(arguments)
+save_to_json_file(add_list, "add_item.json")
