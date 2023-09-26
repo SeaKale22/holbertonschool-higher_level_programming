f4dca89a7946263f0c638ea81c0f5d1f783e56aa
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-26 12:15:13 -0700

    added documentation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,5 +3,10 @@
 
 
 def write_file(file_name="", text=""):
+    """Writes a text file
+    Param file_name: the name of the file to write
+    Param text: text to write into file
+    Returns: Number of characters written
+    """
     with open(file_name, "w", encoding="UTF8") as f:
         return f.write(text)
