17a41e2c21e3cb9a0e9a525853faaea81c5e49e7
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-26 12:46:11 -0700

    created to_json_string to convert ojects to json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Modue for serilizeing string"""
+
+
+import json
+
+
+def to_json_string(obj):
+    """Returns: JSON repenzantation of an object"""
+    return json.dumps(obj)
