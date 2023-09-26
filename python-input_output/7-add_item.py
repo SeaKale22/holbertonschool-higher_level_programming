4b9a40ab14dd2bc0bfc7aa9fcf1f1339b3099c12
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-26 14:44:23 -0700

    used if else to check if file exists

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,8 +11,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 arguments = sys.argv[1:]
 
 if not os.path.exists("add_item.json"):
-    save_to_json_file([], "add_item.json")
-
-add_list = load_from_json_file("add_item.json")
-add_list.extend(arguments)
-save_to_json_file(add_list, "add_item.json")
+    add_list = arguments
+    save_to_json_file(add_list, "add_item.json")
+else:
+    add_list = load_from_json_file("add_item.json")
+    add_list.extend(arguments)
+    save_to_json_file(add_list, "add_item.json")
