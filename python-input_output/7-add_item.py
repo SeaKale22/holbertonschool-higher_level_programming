7239c7bf64257095c88ebf81ba67050689b44be8
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-26 14:21:38 -0700

    added check for if file does not exist

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,12 +3,17 @@
 
 
 import sys
+import os
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arguments = sys.argv[1:]
+file_name = "add_item.json"
 
-add_list = load_from_json_file("add_item.json")
-add_list.append(arguments)
-save_to_json_file(add_list, "add_item.json")
+if not os.path.exists(file_name):
+    save_to_json_file([], file_name)
+
+add_list = load_from_json_file(file_name)
+add_list.extend(arguments)
+save_to_json_file(add_list, file_name)
