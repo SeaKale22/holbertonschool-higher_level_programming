4ca17a68404c15ea2f3613b7289b121a49c53f3f
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-09-26 12:31:43 -0700

    created append_write to append to text file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for appending"""
+
+
+def append_write(file_name="", text=""):
+    """Appends to a file
+    Parm file_name: name of file
+    Param text: text to append
+    """
+    with open(file_name, "a", encoding="UTF8") as f:
+        return f.write(text)
+    
