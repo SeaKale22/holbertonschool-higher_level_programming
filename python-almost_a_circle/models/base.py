08298164f46196aa0327bf679ac6c10b0f1f2a5e
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-02 08:10:25 -0700

    fixed for pycodestyle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,11 +3,14 @@
 
 
 class Base:
+
     """Base class for other project classes"""
     __nb_objects = 0
-    def __init__(self, id =None):
+
+    def __init__(self, id=None):
+
         """Initalization with optional id"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
