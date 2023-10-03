8f0f9fb4f8d3dbb69f30d01da17a92cbbe5289b0
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-03 07:52:37 -0700

    oveloaded update method for Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,3 +26,27 @@ class Square(Rectangle):
         """setter for size"""
         self.width = value
         self.height = value
+    
+    def update(self, *args, **kwargs):
+        """Updates square by assigning argument to each attribute"""
+        if not args:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.width = kwargs["size"]
+                self.height = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+        else:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.width = arg
+                    self.height = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
