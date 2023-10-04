132a25159deb2b3f30a28d7f043887caa179cceb
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-04 08:27:22 -0700

    created test for to_json_string

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,8 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class test_Base(unittest.TestCase):
@@ -24,3 +26,12 @@ class test_Base(unittest.TestCase):
         self.assertEqual(b1.id, 22)
         b2 = Base(45)
         self.assertEqual(b2.id, 45)
+
+class test_Base_to_json_str(unittest.TestCase):
+    """Tests the to_json_string method"""
+    def test_normal(self):
+        """tests for correct useage"""
+        r1 = Rectangle(10, 7, 2, 8, 77)
+        dictionary = r1.to_dictionary()
+        json_dict = Base.to_json_string([dictionary])
+        self.assertEqual(json_dict, '[{"id": 77, "width": 10, "height": 7, "x": 2, "y": 8}]')
