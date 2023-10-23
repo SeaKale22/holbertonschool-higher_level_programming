32da785f436491e2cadba1e9608dd169c37dd4ff
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-23 14:14:09 -0700

    creates new user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- 1-create_user
+-- create user with all privlages
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
