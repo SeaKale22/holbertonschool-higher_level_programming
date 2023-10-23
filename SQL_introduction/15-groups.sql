961ee0a5864c7b227acd5f1390576e58ddaf33af
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-22 17:17:41 -0700

    order by desc

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- 15-groups.sql
 -- Lists number of records with same score
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
