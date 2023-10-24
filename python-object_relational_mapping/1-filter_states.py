db4420fb219a4c8f9318e8e1dd63267848439218
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-24 11:13:54 -0700

    moved location where checking if name is main

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,27 +4,29 @@
 import MySQLdb
 import sys
 
+
+def list_states_N(username, password, database):
+    """Function to list states starting with N"""
+    database_connection = MySQLdb.connect(host='localhost',
+                                          user=username, passwd=password,
+                                          db=database, port=3306)
+    # connects to database
+    cursor = database_connection.cursor()
+    # make a cursor
+    cq = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    cursor.execute(cq)
+    # get the states
+    for row in cursor.fetchall():
+        # print each state
+        print(row)
+
+    cursor.close()
+    database_connection.close()
+    # close cursor and connection to database
+
+
 if __name__ == "__main__":
     # checks if code was not imported
-    def list_states_N(username, password, database):
-        """Function to list states starting with N"""
-        database_connection = MySQLdb.connect(host='localhost',
-                                              user=username, passwd=password,
-                                              db=database, port=3306)
-        # connects to database
-        cursor = database_connection.cursor()
-        # make a cursor
-        cq = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-        cursor.execute(cq)
-        # get the states
-        for row in cursor.fetchall():
-            # print each state
-            print(row)
-
-        cursor.close()
-        database_connection.close()
-        # close cursor and connection to database
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
