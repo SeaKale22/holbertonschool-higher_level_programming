0718e7da0de7c6e67de1c4fa454a24728ee3aa32
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-11-27 13:40:09 -0800

    fixed for semistandard

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,10 +2,10 @@
 
 exports.esrever = function (list) {
   // returns a reversed version of the list
-  let newList = [];
+  const newList = [];
   const listLenght = list.length;
   for (let i = listLenght - 1; i >= 0; i--) {
-    let currentElement = list[i];
+    const currentElement = list[i];
     newList.push(currentElement);
   }
   return newList;
