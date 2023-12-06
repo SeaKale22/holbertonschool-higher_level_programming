c7544c2ae6192b7a854d0e4cfb2f8213a98d0f88
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-12-05 17:11:12 -0800

    fixed for semistandard

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -2,15 +2,15 @@
 const movieList = $('#list_movies');
 
 $.ajax({
-    url: 'https://swapi-api.hbtn.io/api/films/?format=json',
-    method: 'GET',
-    success: function (data) {
-      for (let i = 0; i < data.results.length; i++) {
-        let title = '<li>' + data.results[i].title + '</li>';
-        movieList.append(title);
-      }
-    },
-    error: function (error) {
-        console.log(error);
+  url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+  method: 'GET',
+  success: function (data) {
+    for (let i = 0; i < data.results.length; i++) {
+      const title = '<li>' + data.results[i].title + '</li>';
+      movieList.append(title);
     }
-  });
+  },
+  error: function (error) {
+    console.log(error);
+  }
+});
